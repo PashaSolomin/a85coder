dc26d88c810ce2fa2c2f62c2050694c3a6d6a56e
Соломин Павел <solomin_pasha_cool@mail.ru>
2025-05-12 14:37:51 +0300

    unit-тесты вроде работают, ошибок в программе не найдено

--- unit_test.py
+++ unit_test.py
@@ -5,21 +5,19 @@ import base64
 import unittest
 def test(a, t):
     if t==1:
-        b=a.encode('utf-8')
-        res=base64.a85encode(b)
+        res=base64.a85encode(a)
     if t==2:
-        b=a.encode('utf-8')
-        res=base64.a85decode(b)
+        res=base64.a85decode(a)
     return res
 
 class TestASCII85(unittest.TestCase):
     def test_encode1(self):
-        encoded=base64.a85encode(b"Blue")
-        self.assertEqual(a85e('Blue'), test('Blue', 1))
+        self.assertEqual(a85coder.a85e(b'Blue'), test(b'Blue', 1))
     def test_decode1(self):
-        self.assertEqual(a85d(';K$2ZEZe=p@<<X'), test(';K$2ZEZe=p@<<X', 2))
+        self.assertEqual(a85coder.a85d(b';K$2ZEZe=p@<<X'), test(b';K$2ZEZe=p@<<X', 2))
     def test_encode2(self):
-        encoded=base64.a85encode(b"Blue")
-        self.assertEqual(a85e('Kbpf'), test('Blue', 1))
+        self.assertEqual(a85coder.a85e(b'Kbpf'), test(b'Kbpf', 1))
     def test_decode2(self):
-        self.assertEqual(a85d('DIZ^"DBMG]B-:YnAm5'), test('DIZ^"DBMG]B-:YnAm5', 2))
+        self.assertEqual(a85coder.a85d(b'DIZ^"DBMG]B-:YnAm5'), test(b'DIZ^"DBMG]B-:YnAm5', 2))
+if __name__ == '__main__':
+    unittest.main()
