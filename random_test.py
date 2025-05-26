58b8be1df64aeb4ace340cf3f82c854df91ddfea
Соломин Павел <solomin_pasha_cool@mail.ru>
2025-05-26 15:13:10 +0300

    тесты на случайных корректных и некорректных данных

--- random_test.py
+++ random_test.py
@@ -4,14 +4,16 @@ import sys
 import base64
 data=sys.stdin.buffer.read()
 if a85coder.a85e(data)==base64.a85encode(data):
-    print("incoder ok")
+    print("incoder is ok")
 else:
-    print("incoder not ok")
+    print("incoder is not ok")
 encoded=base64.a85encode(data)
-print(encoded)
+#print(encoded)
 if a85coder.a85d(encoded)==base64.a85decode(encoded):
-    print("decoder ok")
-    print(a85coder.a85d(encoded))
+    print("decoder is ok")
+    #print(a85coder.a85d(encoded))
+    #print("\n")
+    #print(base64.a85decode(encoded))
 else:
-    print("decoder not ok")
+    print("decoder is not ok")
 
--- random_test.sh
+++ random_test.sh
@@ -10,28 +10,8 @@ sys.stdout.buffer.write(base64.a85encode(sys.stdin.buffer.read()))'
  <random.bin >random.a85
 
 
-python3 a85coder.py  <random.bin >random.a85.test
-python3 a85coder.py -d  <random.a85 >random.bin.test
-#echo "random.bin.test (decoded)"
-#cat random.bin.test
-#echo "random.bin (original)"
-#cat random.bin
-
-if ! cmp -s random.a85 random.a85.test; then
-echo "Encoder failed on random data" >&2
-exit 1
-fi
-
-
-if ! cmp -s random.bin random.bin.test; then
-echo "Decoder failed on random data" >&2
-exit 1
-fi
-
-
-echo Ok! >&2
-exit 0
-
+python3 random_test.py  <random.bin >random.a85.test
+cat random.a85.test
 #randombin - байтики
 #random.a85 - закодированные байтики
 #random.bin.test - раскодированные байтики
